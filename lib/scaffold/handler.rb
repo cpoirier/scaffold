6adc5e9084426646772e1c95f83dbe6d30ceda8b
Chris Poirier <chris@courage-my-friend.org>
2011-12-11 08:38:28 -0500

    More of the previous commit.

--- lib/scaffold/handler.rb
+++ lib/scaffold/handler.rb
@@ -23,29 +23,69 @@
 # The fundamental URL handler in the system.
 
 module Scaffold
-module Organization
 class Handler
 
-   def initialize()
-      
+   #
+   # If you'd rather not subclass, you can fill in the Handler by passing a block to call
+   # the private definer methods (your block will be instance_eval'd).
+   
+   def initialize( application, &definer )
+      @application = application
+      instance_eval(&definer) if definer
    end
    
-   #
-   # Various routines that indicate the handler's capabilities.
-   
-   def handles_not_found?()  ; false ; end
-   def handles_index_page?() ; false ; end
-   
    
    #
-   # Returns a handler for the given name, or nil, if this handler doesn't recognize the 
-   # name. As with all Handler operations, you are responsible for your own access control.
+   # Returns a handler and state for the given name, or nil, if this handler doesn't recognize 
+   # the name. As with all Handler operations, you are responsible for your own access control.
    
-   def find( name, context )
-      return nil
+   def resolve( name, state )
+      return defined?(@resolver) ? @resolver.call(name, state) : nil
    end
+   
+
+   #
+   # 
+   
+   def process( route, context )
+   end
+   
+   
+
+
+   
+   #
+   # Returns true if the handler's action can do something with a name it failed to resolve.
+   
+   def handles_not_found?()
+      defined?(@handles_not_found ) && @handles_not_found
+   end
+   
+   
+   #
+   # Returns true if the handler's action can do something with a trailing slash on the
+   # resolved name.
+   
+   def handles_index_page?()
+      defined?(@handles_index_page) && @handles_index_page
+   end
+   
+
+   
+private
+
+   def define_resolver(&block)
+      @resolver = block
+   end
+   
+   def define_action(&block)
+      @action = block
+   end
+
+   attr_writer :handles_not_found, 
+   attr_writer :handles_index_page
+   
    
    
 end # Handler
-end # Organization
 end # Scaffold
--- lib/scaffold/harness/state.rb
+++ lib/scaffold/harness/state.rb
@@ -18,92 +18,150 @@
 #             limitations under the License.
 # =============================================================================================
 
+require Scaffold.locate("rack.rb")
+
+
+
+#
+# Provides the state information against which operations are performed. There should be one 
+# State for each request. Together with Route, its provide the context for each operation.
 
 module Scaffold
+module Harness
+class State
+
+   attr_reader :application, :get_parameters, :post_parameters, :cookies, :status
+   attr_accessor :content_type
+   
+   def secure? ; return !!@secure ; end
+   
 
    #
-   # Provides the state information against which operations are performed.  States cascade
-   # in shells, from request down through the routing layers to you.
+   # +base_url+ should not end in a slash, and is the URL that gets you to the root of the 
+   # application. All absolute paths passed to make_url() will use this as the base.
+   #
+   # +offset_path+ should always start with a slash, and be the rest of the requested URL after 
+   # +base_url+. As a convenience, you can pass the whole URL and State will slice it
+   # appropriately for you.
+   
+   def initialize( application, url, post_parameters = {}, cookies = {}, secure = false )
+      @application      = application
+      @url              = url
 
-   class State
-
-      SOURCE_GET      = 1
-      SOURCE_POST     = 2
-      SOURCE_COOKIES  = 3
-      SOURCE_INTERNAL = 4
+      @secure           = secure
+      @properties       = application.properties.dup
+      @get_parameters   = url.get_parameters
+      @post_parameters  = post_parameters 
+      @cookies          = cookies
+      @cookie_sets      = {}
+      @content_type     = "text/html";
+      @status           = 200;
+      @headers          = []
       
-      def initialize( internal_properties = {} )
-         @internal_properties = internal_properties
-      end
-      
-      attr_reader :request, :response, :url, :unresolved, :handler
-      
-      def get_property( name, default = nil, search_order = SOURCE_INTERNAL, require_https = false )
-         search_order.to_a.member?(SOURCE_INTERNAL) ? get_internal_property(name, default) : default
-      end
-      
-      def get_internal_property( name, default = nil )
-         @internal_properties.fetch(name, default)
-      end
-      
-   end # State
+      load_parameters()
+      instance_eval(&definer) if definer
+   end
    
    
    #
-   # Adds a Request and Response to the State.
+   # Gets a property from the state. State properties are picked up in the following priority: 
+   # direct sets on the state; application properties; post parameters; get parameters; 
+   # cookies; application defaults; your passed default. 
    
-   class RequestState < State
-      
-      def self.build( context_state, rack_env )
-         new(context_state, Rack::Request.new(rack_env), Rack::Response.new(rack_env))
-      end
-      
-      def initialize( context_state, request = nil, response = nil )
-         super( context_state.instance_variable_get(:@internal_properties) )
-         @context_state = context_state
-         @request       = request  || context_state.request
-         @response      = response || context_state.response
-      end
-      
-      def get_property( name, default, search_order = [SOURCE_GET, SOURCE_POST, SOURCE_COOKIES, SOURCE_INTERNAL], require_https = false )
-         value = nil
-         
-         search_order.each do |source|
-            if source == SOURCE_INTERNAL then
-               value = get_internal_property(name, default)
-            elsif !require_https || @request.scheme == "https" then
-               case source
-               when SOURCE_GET   ; value = @request.GET.fetch(name)
-               when SOURCE_POST  ; value = @request.POST.fetch(name)
-               when SOURCE_COOKIE; value = @request.cookies.fetch(name)
-               end
-            end
-            
-            break if value
+   def []( name, default = nil )
+      @properties.fetch(name) do |key|
+         @cookies.fetch(name) do
+            @application.defaults.fetch(key, default)
          end
-         
-         return value || default
       end
+   end
+   
+
+   #
+   # Sets a property in the state.
+   
+   def []=( name, value )
+      @properties[name] = value
+   end
+
+   
+   #
+   # Deletes a property from the state. Note, a subsequent retrieval will return an application 
+   # default at best. Deleting a property that was passed in the get parameters, for instance, 
+   # means you will no longer be able to get that property using the [] routine. You can still 
+   # retrieve it directly from get_parameters(), however.
+   
+   def delete_property( name )
+      @properties.delete(name)
    end
 
 
    #
-   # Adds the Agent and (possibly partial) URL resolution information to the state.
-   
-   class AgentState < RequestState
-      def initialize( context_state, agent, url, unresolved = "" )
-         super( context_state )
-         @handler     = agent
-         @url           = url
-         @unresolved    = unresolved
-         @context_state = context_state
-      end
-      
-      def get_internal_property( name, default = nil )
-         @handler.get_property(name, default) || super
-      end
+   # Sets a cookie into the state and client.
+
+   def set_cookie( name, value, expires_in = 0 )
+      @cookies[name] = value
+      @cookie_sets[name] = CookieSet.new(name, value, expires_in)
    end
    
 
+   #
+   # Unsets a cookie from the state and client.
+   
+   def unset_cookie( name )
+      @cookies.delete(name)
+      @cookie_sets[name] = CookieSet.new(name, "", -1)
+   end
+   
+   
+   #
+   # Adds a header to the response.
+   
+   def add_header( name, value )
+      @headers << "#{name}: #{value}"
+   end
 
+
+   #
+   # Discards all headers already set. Does not affect cookie sets unless you say it should.
+   # Note: even if you discard cookies, their values will not be removed from the state 
+   # properties.
+   
+   def reset_headers( and_cookies = false )
+      @headers.clear
+      @cookie_sets.clear if and_cookies
+   end
+   
+   
+   #
+   # Forks off a state for a secondary (internal) request, backed by the context in this one. 
+   # By default, cookies are passed through from this request to the new one. If you don't 
+   # want that, pass an empty hash for +cookies+. By default, forked requests inherit the 
+   # secure-ness of the this State. If you want a specific value, set +secure+ appropriately. 
+   # Forked requests do not inherit either get or post parameters, so pass those accordingly.
+   
+   def fork( path, get_parameters = {}, cookies = nil, secure = nil, post_parameters = {} )
+      cookies ||= @cookies
+      secure = @secure if secure.nil?
+      url    = @url.offset(path, get_parameters, false)
+      
+      new(application, url, post_parameters, cookies, secure)
+   end
+      
+
+private
+
+   def load_parameters()
+      [@post_parameters, @get_parameters].each do |set|
+         set.each do |key, value|
+            @properties[key] = value unless @properties.member?(key)
+         end
+      end
+   end
+
+
+   CookieSet = Struct.new(:name, :value, :expires_in)
+
+end # State
+end # Harness
 end # Scaffold
